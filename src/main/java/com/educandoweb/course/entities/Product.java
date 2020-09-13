a6d856537d5de9c3772d13796db3303eb2afa121
Alan Badan <badanalan@hotmail.com>
2020-09-13 19:22:44 -0300

    OrderItem

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -14,7 +14,6 @@ import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
-
 @Table(name = "tb_product")
 public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
