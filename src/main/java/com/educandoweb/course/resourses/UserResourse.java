81d7053fffb130365a2a61ce55b8e20c0fe2d574
Alan Badan <badanalan@hotmail.com>
2020-09-13 12:06:02 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/educandoweb/course/resourses/UserResourse.java
+++ src/main/java/com/educandoweb/course/resourses/UserResourse.java
@@ -20,8 +20,7 @@ public class UserResourse {
 	private UserServices service;
 	
      @GetMapping
-	public ResponseEntity<List> findAll() {
-    	 
+	public ResponseEntity<List<User>> findAll() {
 		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
